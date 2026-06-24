25e3464ad77b751d96c29bfb45e3cc7146af977b
Anton Olguin <antonolguin1@gmail.com>
2026-06-24 09:48:17 -0600

    set up player

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkeypress(key="Up", fun=player.move_up)
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
